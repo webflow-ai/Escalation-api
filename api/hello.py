3891b84d1355f961bf7a985272f602cfc1a7550a
Tripagra <tripagrateam@gmail.com>
2026-04-13 13:50:28 +0530

    Add test endpoint to verify Python functions work

---
+++ api/hello.py
@@ -0,0 +1,27 @@
+"""
+Simple test endpoint to verify Python functions work on Vercel
+"""
+
+from http.server import BaseHTTPRequestHandler
+import json
+
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-Type', 'application/json')
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.end_headers()
+        
+        response = {
+            "message": "Hello from Python serverless function!",
+            "status": "working"
+        }
+        
+        self.wfile.write(json.dumps(response).encode('utf-8'))
+    
+    def do_OPTIONS(self):
+        self.send_response(200)
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.send_header('Access-Control-Allow-Methods', 'GET, OPTIONS')
+        self.send_header('Access-Control-Allow-Headers', 'Content-Type')
+        self.end_headers()
